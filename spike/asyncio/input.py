cfff0d94a24e5059ce2a78efd5658eeb7ac96050
Sarbjit Singh <srbcheema1@gmail.com>
2018-07-17 17:30:32 +0530

    finally got something that could read stdin spike/asyncio/input.py

--- spike/asyncio/input.py
+++ spike/asyncio/input.py
@@ -9,8 +9,15 @@ async def read_input():
             if not inp: break
             print('got :' , inp)
 
+async def read_stdin():
+    async with aiofiles.open('/dev/stdin', mode='rb') as f:
+        while True:
+            inp = await f.read(4)
+            if not inp: break
+            print('got :' , inp)
+
 async def main():
-    await asyncio.wait([read_input()])
+    await asyncio.wait([read_stdin()])
 
 event_loop = asyncio.get_event_loop()
 event_loop.run_until_complete(main())
